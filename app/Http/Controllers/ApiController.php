bf699d419282259b40086442742ee2bdca96de40
OEM Configuration (temporary user) <sidiqkeren31@gmail.com>
2021-08-30 15:55:39 +0700

    update 30 agustus sore

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pengukuran;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function cpeng(request $request)
+    {
+        $input = new Pengukuran;
+        $input->suhu = $request->suhu;
+        $input->arah_angin = $request->arah_angin;
+        $input->curah_hujan = $request->curah_hujan;
+        $input->tekanan_udara = $request->tekanan_udara;
+        $input->kelembaban = $request->kelembaban;
+        $input->save();
+    }
+
+    public function speng()
+    {
+     return Pengukuran::all();
+    }
 }
